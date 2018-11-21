8646d21f05bf6fbc26bae75f4d36791a2c3561f6
Burdzi0 <burdzi0@programmer.net>
2018-11-21 18:53:51 +0100

    Create Publisher entity

--- src/main/java/com/burdzi0/spring5webapp/model/Book.java
+++ src/main/java/com/burdzi0/spring5webapp/model/Book.java
@@ -13,7 +13,6 @@ public class Book {
 
 	private String title;
 	private String isbn;
-	private String publisher;
 
 	@ManyToMany
 	@JoinTable(name = "author_book",
@@ -22,20 +21,23 @@ public class Book {
 	)
 	private Set<Author> authors = new HashSet<>();
 
+	@OneToOne
+	private Publisher publisher;
+
 	public Book() {
 	}
 
-	public Book(String title, String isbn, String publisher) {
+	public Book(String title, String isbn, Publisher publisher) {
 		this.title = title;
 		this.isbn = isbn;
 		this.publisher = publisher;
 	}
 
-	public Book(String title, String isbn, String publisher, Set<Author> authors) {
+	public Book(String title, String isbn, Set<Author> authors, Publisher publisher) {
 		this.title = title;
 		this.isbn = isbn;
-		this.publisher = publisher;
 		this.authors = authors;
+		this.publisher = publisher;
 	}
 
 	public Long getId() {
@@ -62,11 +64,11 @@ public class Book {
 		this.isbn = isbn;
 	}
 
-	public String getPublisher() {
+	public Publisher getPublisher() {
 		return publisher;
 	}
 
-	public void setPublisher(String publisher) {
+	public void setPublisher(Publisher publisher) {
 		this.publisher = publisher;
 	}
 
