b6913d01f53a61a912849dfd601029552f07c247
Burdzi0 <burdzi0@programmer.net>
2018-11-21 18:46:26 +0100

    Add repositories to save entities

--- src/main/java/com/burdzi0/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/com/burdzi0/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,10 +2,22 @@ package com.burdzi0.spring5webapp.bootstrap;
 
 import com.burdzi0.spring5webapp.model.Author;
 import com.burdzi0.spring5webapp.model.Book;
+import com.burdzi0.spring5webapp.repositories.AuthorRepository;
+import com.burdzi0.spring5webapp.repositories.BookRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
 
+@Component
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+	private AuthorRepository authorRepository;
+	private BookRepository bookRepository;
+
+	public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+		this.authorRepository = authorRepository;
+		this.bookRepository = bookRepository;
+	}
 
 	@Override
 	public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
@@ -15,13 +27,21 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 	private void initData() {
 		Author eric = new Author("Eric","Err");
 		Book book = new Book("Domain Driven Development", "12345", "HarperCollins");
-
 		eric.getBooks().add(book);
 		book.getAuthors().add(eric);
 
+		authorRepository.save(eric);
+		bookRepository.save(book);
+
+
 		Author rod = new Author("Rod","Johnson");
 		Book j2ee = new Book("J2EE", "12346", "Apress");
+
 		rod.getBooks().add(j2ee);
 		j2ee.getAuthors().add(rod);
+
+		authorRepository.save(rod);
+		bookRepository.save(j2ee);
+
 	}
 }
